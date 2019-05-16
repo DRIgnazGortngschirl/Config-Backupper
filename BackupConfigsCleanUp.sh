d692291f8dcd83b502ffbaa2788021ced94299ef
Mario Michel <1108mario@gmail.com>
2019-05-16 14:58:31 +0200

    Update BackupConfigsCleanUp.sh

--- BackupConfigsCleanUp.sh
+++ BackupConfigsCleanUp.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "All backuped backups will get deleted!"
-read -p "Do you want to continue ?" -n 1 -r
+read -p "Do you want to continue ? Y/n : " -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Y]$ ]]
-then
-    read -p "I will delete all Backuped Configs. Hope you got a backup ;)" -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Y]$ ]]
-then
-    echo "I will DELETE now ALL Backuped Configs."
-sleep 3
-cd ../
-find ./Archiv -maxdepth 2 -type f -exec rm -fv {} \;
-fi
+    then
+        read -p "I will delete all Backuped Configs. Hope you got a backup ;) Y/n : " -n 1 -r
+        echo 
+        if [[ $REPLY =~ ^[Y]$ ]]
+            then
+                echo "I will DELETE now ALL Backuped Configs."
+                sleep 3
+                find ./Archiv -maxdepth 2 -type f -exec rm -fv {} \;
+        fi
 fi
