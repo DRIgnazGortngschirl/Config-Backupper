65ffdaa58c744775bd75858d3e82d096dd9e0a8f
Mario Michel <1108mario@gmail.com>
2019-06-14 09:48:16 +0200

    Update Checker.sh

--- Checker.sh
+++ Checker.sh
@@ -4,9 +4,9 @@ fortinethostfilecount=`egrep -v "^\s*(#|$)" ./Devices/Fortinet/Fortinet-Devices.
 hphostfilecount=`egrep -v "^\s*(#|$)" ./Devices/HP/HP-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l`
 ciscohostfilecount=`egrep -v "^\s*(#|$)" ./Devices/Cisco/Cisco-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l`
 dellhostfilecount=`egrep -v "^\s*(#|$)" ./Devices/DELL/DELL-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l`
-dirsinachriv=`find ./Archiv -maxdepth 1 -type d | wc -l`
-configsinachive=`find ./Archiv -maxdepth 2 -type f -mmin -120 | wc -l`
-minus1=1 # Caused by also counting the ./Archiv as a folder 
+dirsinachriv=`find ./Archive -maxdepth 1 -type d | wc -l`
+configsinachive=`find ./Archive -maxdepth 2 -type f -mmin -120 | wc -l`
+minus1=1 # Caused by also counting the ./Archive as a folder 
 
 total=`expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount`
 dirsinachriv=`expr $dirsinachriv - $minus1`
@@ -19,9 +19,9 @@ echo "DELL ...... : $dellhostfilecount Hosts in Host File"
 echo "------------------------------------------------"
 echo "Lines in Host file .......... : $total"
 echo "Directorys in Archiv ........ : $dirsinachriv"
-echo "Current Configs in Achive ... : $configsinachive"
+echo "Current Configs in Archive ... : $configsinachive"
 echo "------------------------------------------------"
-echo "(Lines in Host file) & (Current Configs in Achive) These numbers should match!!!"
+echo "(Lines in Host file) & (Current Configs in Archive) These numbers should match!!!"
 echo "Otherwise you didn't got all configs listed in the host files"
 echo "Start Fastdebug.sh to see more infos where those errors are"
-echo "Remidner: If you got more (Directorys in Archiv) than (Lines in Host file) the could be caused by a switch over from e.g. fw-01 to fw-02"
+echo "Remidner: If you got more (Directorys in Archive) than (Lines in Host file) the could be caused by a switch over from e.g. fw-01 to fw-02"
