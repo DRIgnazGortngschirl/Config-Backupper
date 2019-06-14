92356e537834ddbbe1801a04597db59088dc64d4
Mario Michel <1108mario@gmail.com>
2019-06-14 09:52:22 +0200

    Update Fortinet.sh

--- Fortinet.sh
+++ Fortinet.sh
@@ -14,6 +14,6 @@ do
     echo -e "Host found in hostfile --> $device"
     scp -v -i ./SSH-Keys/Backup-SSH-Key $user@$device:sys_config ./BackupConfigFortinet # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
     name=`pv BackupConfigFortinet | grep -m1 'set hostname' | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'` # Will search for the host name to set create a directory and a file named like the hostname of the network device 
-    mkdir -v Archiv/$name
-    mv -v BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H-%M_%d-%m-%Y").conf # Will move the copied file to the correct folder and will also rename it with the hostname
+    mkdir -v Archive/$name
+    mv -v BackupConfigFortinet ./Archive/$name/$name-$(date +"%H-%M_%d-%m-%Y").conf # Will move the copied file to the correct folder and will also rename it with the hostname
 done
