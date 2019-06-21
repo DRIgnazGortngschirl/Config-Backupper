ac27e0d092196448d819b2af403c1240e97997b9
Mario Michel <1108mario@gmail.com>
2019-06-21 19:56:29 +0200

    Update WebsiteIndex.sh

--- Modules/Setup/WebsiteIndex.sh
+++ Modules/Setup/WebsiteIndex.sh
@@ -20,8 +20,8 @@ if [[ $REPLY =~ ^[Y]$ ]]
             then
                 echo "I will symlink them now"
                 sleep 3
-                sudo mkdir -v -p $webserverpath
-                sudo ln -sf $installpath/Config-Backupper/Archive/ $webserverpath
+                mkdir -v -p $webserverpath
+                ln -sf $installpath/Archive $webserverpath
                 subfolder=`readlink -f $webserverpath | grep -oE '[^/]+$'`
                 echo "Open http|https://$ip/$subfolder/index.php"
         fi
