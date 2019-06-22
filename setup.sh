7a788faafc23bd0853a25aab10a6639e26c900dc
Mario Michel <1108mario@gmail.com>
2019-06-22 15:33:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 # SETUP
 echo "[i] : Setup stared"
 
-# Phase 1 create Main-Lanucher.sh
+# Phase 1 remove ./.git
+rm -v ./.git -Rf
+echo "[i] : Removed ./.git"
+
+# Phase 2 create Main-Lanucher.sh
 echo "[i] : Searching for installation path ... This can take a few moments"
 installpath=`find / -name "*Config-Backupper" 2>/dev/null`
 echo "Installation path : $installpath"
@@ -31,7 +35,7 @@ echo "find ./Log -mtime +$archivetimelogsdelete -exec gzip -v {} +" >> ./Modules
 echo "--------------------------------------------------------------------------------"
 echo "[i] : Main Launcher where created"
 
-# Phase 2 make the files executable
+# Phase 3 make the files executable
 chmod --verbose 700 ./Main-Launcher.sh
 chmod --verbose 700 ./Modules/Backup/Fortinet/Fortinet.sh
 chmod --verbose 700 ./Modules/Backup/Fortinet/Fortinet-Special.sh
@@ -49,7 +53,7 @@ chmod --verbose 700 ./Modules/Setup/Fortinet/AutoSetup.sh
 chmod --verbose 700 ./Modules/Setup/WebsiteIndex.sh
 echo "[i] : Modules & Lanucher where modified"
 
-# Phase 3 create SSH Key
+# Phase 4 create SSH Key
 echo "----------------------------------------"
 echo '1024 bit - ONLY FOR TESTING'
 echo '2048 bit - It is "secure" until 2030!'
@@ -61,27 +65,27 @@ echo "Enter bit lenth (Numbers only):"
 read rsakeylenth
 ssh-keygen -t rsa -b $rsakeylenth -f ./SSH-Keys/Backup-SSH-Key
 
-# Phase 4 show the new created Public SSH-Key
+# Phase 5 show the new created Public SSH-Key
 echo "-----BEGIN PUBLIC KEY-----"
 echo "$(cat ./SSH-Keys/Backup-SSH-Key.pub)"
 echo "-----END PUBLIC KEY-----"
 echo " "
 
-# Phase 5 configure Fortinet-AutoSetup.sh
+# Phase 6 configure Fortinet-AutoSetup.sh
 search=PLACEHOLDERFORSSHKEY
 replace=`cat ./SSH-Keys/Backup-SSH-Key.pub`
 sed -i "s|${search}|${replace}|g" ./Modules/Setup/Fortinet/AutoSetup.sh
 echo '[i] : Fortinet Fortinet-AutoSetup.sh where configured'
 
-# Phase 6 show e.g. for a crontab
+# Phase 7 show e.g. for a crontab
 echo "--------------------------------------------------------------------------------"
 echo "Create a crontab to run the backup every day @ 2:00 enter this line in crontab"
 echo "0 2 * * * $installpath/Main-Launcher.sh"
 echo "--------------------------------------------------------------------------------"
 
-# Phase 7 remove setup.sh & .git/
-rm -v .git -Rf
-echo "[i] : Removed ./.git"
-rm -v setup.sh
+# Phase 8 remove ./Log/<VENDOR>/DeleteMe files
+rm ./Log/Cisco/DeleteMe ./Log/DELL/DeleteMe ./Log/Fortinet/DeleteMe ./Log/HP/DeleteMe
+echo "[i] : Removed ./Log/<VENDOR>/DeleteMe files"
+rm -v ./setup.sh
 echo "[i] : Removed ./setup.sh"
 echo "[i] : IT'S DONE !"
