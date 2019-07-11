d7f6d5a2ba61119a11d709191bc7f17400668094
Mario Michel <1108mario@gmail.com>
2019-07-11 19:52:03 +0200

    Update Cisco.sh

--- Modules/Backup/Cisco/Cisco.sh
+++ Modules/Backup/Cisco/Cisco.sh
@@ -1,12 +1,3 @@
-#  _    _ _   _ _____  ______ _____     _____ ____  _   _  _____ _______ _____  _    _  _____ _______ _____ ____  _   _
-# | |  | | \ | |  __ \|  ____|  __ \   / ____/ __ \| \ | |/ ____|__   __|  __ \| |  | |/ ____|__   __|_   _/ __ \| \ | |
-# | |  | |  \| | |  | | |__  | |__) | | |   | |  | |  \| | (___    | |  | |__) | |  | | |       | |    | || |  | |  \| |
-# | |  | | . ` | |  | |  __| |  _  /  | |   | |  | | . ` |\___ \   | |  |  _  /| |  | | |       | |    | || |  | | . ` |
-# | |__| | |\  | |__| | |____| | \ \  | |___| |__| | |\  |____) |  | |  | | \ \| |__| | |____   | |   _| || |__| | |\  |
-#  \____/|_| \_|_____/|______|_|  \_\  \_____\____/|_| \_|_____/   |_|  |_|  \_\\____/ \_____|  |_|  |_____\____/|_| \_|
-
-# Still working on this. (NOT YET FUNCTIONAL!)
-
 #!/bin/bash
 
 user=backup
@@ -14,10 +5,20 @@ user=backup
 echo "Started Backup of Config's"
 
 for device in `cat ./Devices/Cisco/Cisco-Devices.txt| egrep -v "^\s*(#|$)"` # Will have a look in the file "cisco-devices.txt" for all Cisco devices
-do
-  if nc $device 22 < /dev/null ; then
-      echo "ssh"
-  else
-      ./sgX00.sh $device
-  fi
+ do
+  echo -e "Host --> $device"
+  if ping -c 3 $device &> /dev/null
+   then
+    echo "[i]: $device reachable ✓"
+    ./sgX00.sh $device
+    date=`date +"%H-%M_%d-%m-%Y"`
+    if [ -f ./Archive/$name/$name-$date.conf ]
+     then
+      echo "[i]: File $name-$date.conf found!"
+      echo "[i]: Backup status : succeeded ✓"
+     else
+      echo "[i]: File $name-$date.conf not found!"
+      echo "[i]: Backup status : failed ✘"
+    fi
+ fi
 done
