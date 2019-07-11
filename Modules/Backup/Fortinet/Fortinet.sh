131b38cfb61812b8b2c908b01733fc15b37ba1d4
Mario Michel <1108mario@gmail.com>
2019-07-11 19:28:02 +0200

    Update Fortinet.sh

--- Modules/Backup/Fortinet/Fortinet.sh
+++ Modules/Backup/Fortinet/Fortinet.sh
@@ -2,13 +2,28 @@
 
 user=backup
 
-echo "Started Backup of Config's"
+echo "Started Backup of Configs : FORTINET"
 
 for device in `cat ./Devices/Fortinet/Fortinet-Devices.txt | egrep -v "^\s*(#|$)" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
-do
-    echo -e "Host found in hostfile --> $device"
+ do
+  echo -e "Host --> $device"
+  if ping -c 3 $device &> /dev/null
+   then
+    echo "[i]: $device reachable ✓"
     scp -v -i ./SSH-Keys/Backup-SSH-Key $user@$device:sys_config ./BackupConfigFortinet
     name=`pv BackupConfigFortinet | grep -m1 'set hostname' | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'`
     mkdir -v Archive/$name
-    mv -v BackupConfigFortinet ./Archive/$name/$name-$(date +"%H-%M_%d-%m-%Y").conf
+    date=`date +"%H-%M_%d-%m-%Y"`
+    mv -v BackupConfigFortinet ./Archive/$name/$name-$date.conf
+    if [ -f ./Archive/$name/$name-$date.conf ]
+     then
+      echo "[i]: File $name-$date.conf found!"
+      echo "[i]: Backup status : succeeded ✓"
+     else
+      echo "[i]: File $name-$date.conf not found!"
+      echo "[i]: Backup status : failed ✘"
+    fi
+   else
+    echo "[i]: $HOST not reachable"
+  fi
 done
