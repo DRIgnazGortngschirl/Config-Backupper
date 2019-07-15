4addd186e47665f709a9127ef61483c429846c54
Mario Michel <1108mario@gmail.com>
2019-07-15 22:16:15 +0200

    Update Cisco.sh

--- Modules/Backup/Cisco/Cisco.sh
+++ Modules/Backup/Cisco/Cisco.sh
@@ -4,21 +4,24 @@ user=backup
 
 echo "Started Backup of Config's"
 
-for device in `cat ./Devices/Cisco/Cisco-Devices.txt| egrep -v "^\s*(#|$)" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
+for device in `cat ./Devices/Cisco/Cisco-Devices.txt | egrep -v "^\s*(#|$)" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
  do
   echo -e "Host --> $device"
   if ping -c 3 $device &> /dev/null
    then
     echo "[i]: $device reachable"
     ./sgX00.sh $device
+    name=`cat BackupConfigCisco | grep hostname | sed 's|["?]||g' | sed 's/hostname //'`
+    mkdir -v Archive/$name
     date=`date +"%H-%M_%d-%m-%Y"`
+    mv -v BackupConfigCisco ./Archive/$name/$name-$date.conf
     if [ -f ./Archive/$name/$name-$date.conf ]
      then
       echo "[i]: File $name-$date.conf found!"
-      echo "[i]: Backup status : succeeded"
+      echo "[i]: Backup of $device succeeded"
      else
       echo "[i]: File $name-$date.conf not found!"
-      echo "[i]: Backup status : failed"
+      echo "[i]: Backup of $device failed"
     fi
    else
      echo "[i]: $device not reachable"
