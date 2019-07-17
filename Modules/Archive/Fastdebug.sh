2dc85bd7c691a9ef795f0fa8be36711e36b25de5
Mario Michel <1108mario@gmail.com>
2019-07-17 09:45:04 +0200

    Update Fastdebug.sh

--- Modules/Archive/Fastdebug.sh
+++ Modules/Archive/Fastdebug.sh
@@ -4,11 +4,11 @@ date=`date +%d%m%y`
 
 echo "Fortinet :"
 echo "----------------------------------------------------------------------------"
-cat ./Log/Fortinet/log$date.txt | grep '\[i]:' | tac | sed '/\[i]: Backup status : succeeded/I,+3 d' | tac | grep 'failed' | sed -e 's/\[i]: //' | sed -e 's/backup status :/-->/'
+join <(sort ./Devices/Fortinet/Fortinet-Devices.txt) <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
 echo "----------------------------------------------------------------------------"
 echo "Cisco :"
 echo "----------------------------------------------------------------------------"
-cat ./Log/Cisco/log$date.txt | grep '\[i]:' | tac | sed '/\[i]: Backup status : succeeded/I,+3 d' | tac | grep 'failed' | sed -e 's/\[i]: //' | sed -e 's/backup status :/-->/'
+join <(sort ./Devices/Cisco/Cisco-Devices.txt) <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
 echo "----------------------------------------------------------------------------"
 echo "DELL :"
 echo "----------------------------------------------------------------------------"
