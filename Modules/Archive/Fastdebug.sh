050cc3573c13f4affb227d956a6d01aceea4806e
Mario Michel <1108mario@gmail.com>
2019-07-24 13:13:18 +0200

    Update Fastdebug.sh

--- Modules/Archive/Fastdebug.sh
+++ Modules/Archive/Fastdebug.sh
@@ -2,20 +2,20 @@
 
 date=`date +%d%m%y`
 
-echo "Fortinet :"
-echo "----------------------------------------------------------------------------"
+echo "<h4>Backup Errors</h4>"
+echo "Fortinet:"
+echo "--------------------------------------------------------------------------------------------------"
 join <(sort ./Devices/Fortinet/Fortinet-Devices.txt) <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
-echo "----------------------------------------------------------------------------"
-echo "Cisco :"
-echo "----------------------------------------------------------------------------"
+join <(sort ./Modules/Backup/Fortinet-Special.sh | grep "#" | grep -v '#!/bin/bash' |  sed 's/# //') <(sort ./Log/Fortinet/log$date.txt| grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
+echo "--------------------------------------------------------------------------------------------------"
+echo "Cisco:"
+echo "--------------------------------------------------------------------------------------------------"
 join <(sort ./Devices/Cisco/Cisco-Devices.txt) <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
-echo "----------------------------------------------------------------------------"
-echo "DELL :"
-echo "----------------------------------------------------------------------------"
-# cat ./Log/Cisco/log$date.txt | grep '\[i]:' | tac | sed '/\[i]: Backup status : succeeded/I,+3 d' | tac | grep 'failed' | sed -e 's/\[i]: //' | sed -e 's/backup status :/-->/'
-echo "----------------------------------------------------------------------------"
-echo "HP :"
-echo "----------------------------------------------------------------------------"
-# cat ./Log/Cisco/log$date.txt | grep '\[i]:' | tac | sed '/\[i]: Backup status : succeeded/I,+3 d' | tac | grep 'failed' | sed -e 's/\[i]: //' | sed -e 's/backup status :/-->/'
-echo "----------------------------------------------------------------------------"
-echo "Check the log file from the $date for the error that occurred"
+echo "--------------------------------------------------------------------------------------------------"
+#echo "DELL :"
+#echo "----------------------------------------------------------------------------"
+#cat ./Log/DELL/log$date.txt | grep mv -n |  awk '{print $1}' | cut -f1 -d":"
+#echo "----------------------------------------------------------------------------"
+#echo "HP :"
+#echo "----------------------------------------------------------------------------"
+#cat ./Log/HP/log$date.txt | grep mv -n |  awk '{print $1}' | cut -f1 -d":"
