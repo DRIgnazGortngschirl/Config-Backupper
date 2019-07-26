328a444429a28e59b36b6a8fcc108afc5f938761
Mario Michel <1108mario@gmail.com>
2019-07-26 11:05:05 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 date=`date +%d%m%y`
-echo "Current size of ./Archive : $(du -sh ./Archive)"
-echo "Current configs in ./Archive $(tree | grep .conf | wc -l)" 
+
+totallines=`find ./Archive -name "*.conf" -mmin -820 -exec cat {} + | wc -l`
+configsinarchive=`find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -820 | wc -l`
+avglinesinconfig=`expr $totallines / $configsinarchive`
+currentsizearchive=`du -sh ./Archive | awk '{print $1}'`
+totalconfigsarchive=`tree | grep .conf | wc -l`  
+
+echo "Current size of ./Archive: .......... $currentsizearchive"                   
+echo "Current configs in ./Archive: ....... $totalconfigsarchive"        
+echo "Total lines operating firewalls: .... $totallines"
+echo "Average lines in conifig file: ...... $avglinesinconfig"
