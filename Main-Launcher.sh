f2361e79a437ba5cac21ee00eecb6f3da791d398
Mario Michel <1108mario@gmail.com>
2019-07-27 14:26:35 +0200

    Update Main-Launcher.sh

--- Main-Launcher.sh
+++ Main-Launcher.sh
@@ -4,11 +4,11 @@ date=`date +%d%m%y`
 cd PLACEHOLDERFORINSTALLATIONPATH
 # ---------------------------------------------
 ### Backup Modules ###
-./Modules/Backup/Fortinet/Fortinet.sh &>> ./Log/Fortinet/log$date.txt
-#./Modules/Backup/Fortinet/Fortinet-Special.sh &>> ./Log/Fortinet/log$date.txt ### Disabled by default ###
-./Modules/Backup/Cisco/Cisco.sh &>> ./Log/Cisco/log$date.txt ### Only for sgx00 series working ###
-#./Modules/Backup/DELL/DELL.sh &>> ./Log/DELL/log$date.txt ### NOT YET FUNCTIONAL! ###
-#./Modules/Backup/HP/HP.sh &>> ./Log/HP/log$date.txt ### NOT YET FUNCTIONAL! ###
+# ./Modules/Backup/Fortinet/Fortinet.sh &>> ./Log/Fortinet/log$date.txt ### FUNCTIONAL ###
+# ./Modules/Backup/Fortinet/Fortinet-Special.sh &>> ./Log/Fortinet/log$date.txt ### Disabled by default FUNCTIONAL ###
+# ./Modules/Backup/Cisco/Cisco.sh &>> ./Log/Cisco/log$date.txt ### Only for sgx00 series working ###
+# ./Modules/Backup/DELL/DELL.sh &>> ./Log/DELL/log$date.txt ### NOT YET FUNCTIONAL! ###
+# ./Modules/Backup/HP/HP.sh &>> ./Log/HP/log$date.txt ### NOT YET FUNCTIONAL! ###
 
 ### Archive Compression/Stats/Debugs & Loging ###
 
@@ -18,10 +18,9 @@ cd PLACEHOLDERFORINSTALLATIONPATH
 
 # Stats
 ./Modules/Archive/ArchiveStats.sh &> ./Log/Backup/log$date.txt ### Used for displaying archive stats ###
+# ./Modules/FirmwareCheck/Fortinet/FirmwareChecker.sh > ./Devices/Firmware-Versions/Fortinet-Firmware-$date.txt ### Disabled by default ###
+
 
 # Debug
 ./Modules/Archive/Checker.sh &> ./Log/Backup/log$date.txt ### Used for config copy debugging ###
 ./Modules/Archive/Fastdebug.sh > ./Log/Failed/Failed-$date.txt ### Used for check failed backup attempts 
-
-#./Modules/FirmwareCheck/Fortinet/FirmwareChecker.sh | sed $'s/<br>/\\\n/g' | sed 'N;s/\n/ /' > ./Devices/Firmware-Versions/Fortinet-Firmware-$date.txt ### Disabled by default ###
-
