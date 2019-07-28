5e553c0c6bc2e15666cd42633ea7d7e9acedf212
Mario Michel <1108mario@gmail.com>
2019-07-28 19:26:38 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 date=`date +%d%m%y`
 
-totallines=`find ./Archive -name "*.conf" -mmin -820 -exec cat {} + | wc -l`
-configsinarchive=`find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -820 | wc -l`
+totallines=`find ./Archive -name "*.conf" -mmin -120 -exec cat {} + | wc -l`
+configsinarchive=`find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -120 | wc -l`
 avglinesinconfig=`expr $totallines / $configsinarchive`
 currentsizearchive=`du -sh ./Archive | awk '{print $1}'`B
 totalconfigsarchive=`tree | grep .conf | wc -l`  
