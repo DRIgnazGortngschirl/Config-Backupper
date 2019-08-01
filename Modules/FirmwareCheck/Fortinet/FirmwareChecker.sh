26bb9c87013a9d94bd50ce3fc75e7902e0013160
Mario Michel <1108mario@gmail.com>
2019-08-01 11:14:19 +0200

    Update FirmwareChecker.sh

--- Modules/FirmwareCheck/Fortinet/FirmwareChecker.sh
+++ Modules/FirmwareCheck/Fortinet/FirmwareChecker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ./Archive/ -name "*.conf" -mtime 1 -exec head -n 100 {} + | grep -e '#config-version=' -e 'set hostname' | tr -d " \t" | sed '/\\/d' | sed '/\*/d' | sed 's/\<sethostname\>//g' | sed 's|["?]||g' | sed 's/#config-version=//' | sed 's/:opmode.*//g' | sed $'s/<br>/\\\n/g' | sed 'N;s/\n/ /'
+find ./Archive/ -name "*.conf" -mtime 1 -exec head -n 100 {} + | grep -e '#config-version=' -e 'set hostname' | tr -d " \t" | sed '/\\/d' | sed '/\*/d' | sed 's/\<sethostname\>//g' | sed 's|["?]||g' | sed 's/#config-version=//' | sed 's/:opmode.*//g' | sed $'s/<br>/\\\n/g' | sed 'N;s/\n/ /' | awk '{ t=$1; $1=$NF; $NF=t; print}' | sort
