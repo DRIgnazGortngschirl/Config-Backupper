5b513be8f91be80c403d32be5dd5b23e512ff4e9
Mario Michel <1108mario@gmail.com>
2019-08-22 11:42:05 +0200

    Update Fastdebug.sh

--- Modules/Archive/Fastdebug.sh
+++ Modules/Archive/Fastdebug.sh
@@ -5,12 +5,12 @@ date=`date +%d%m%y`
 echo "Backup Errors"
 echo "Fortinet:"
 echo "--------------------------------------------------------------------------------------------------"
-join <(sort ./Devices/Fortinet/Fortinet-Devices.txt) <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
-# join <(sort ./Modules/Backup/Fortinet-Special.sh | grep "#" | grep -v '#!/bin/bash' |  sed 's/# //') <(sort ./Log/Fortinet/log$date.txt| grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
+join <(sort ./Devices/Fortinet/Fortinet-Devices.txt) <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
+# join <(sort ./Modules/Backup/Fortinet-Special.sh | grep "#" | grep -v '#!/bin/bash' |  sed 's/# //') <(sort ./Log/Fortinet/log$date.txt| grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
 echo "--------------------------------------------------------------------------------------------------"
 echo "Cisco:"
 echo "--------------------------------------------------------------------------------------------------"
-join <(sort ./Devices/Cisco/Cisco-Devices.txt) <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
+join <(sort ./Devices/Cisco/Cisco-Devices.txt) <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "failed" | sed -e 's/\[i]: //')
 echo "--------------------------------------------------------------------------------------------------"
 #echo "DELL :"
 #echo "----------------------------------------------------------------------------"
