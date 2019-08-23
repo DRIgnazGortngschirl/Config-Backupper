99fa5230913b2e7948bb03b3ad3e17d0cf9c2237
Mario Michel <1108mario@gmail.com>
2019-08-23 13:31:14 +0200

    Update Fastdebug.sh

--- Modules/Archive/Fastdebug.sh
+++ Modules/Archive/Fastdebug.sh
@@ -6,7 +6,7 @@ echo "Backup Errors"
 echo "Fortinet:"
 echo "--------------------------------------------------------------------------------------------------"
 join <(sort ./Devices/Fortinet/Fortinet-Devices.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
-# join <(sort ./Modules/Backup/Fortinet-Special.sh | grep '#' | grep -v '#!/bin/bash' | sed 's/# //') <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //'
+# join <(sort ./Modules/Backup/Fortinet-Special.sh | grep '#' | grep -v '#!/bin/bash' | sed 's/# //') <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
 echo "--------------------------------------------------------------------------------------------------"
 echo "Cisco:"
 echo "--------------------------------------------------------------------------------------------------"
