048ff76dfbbf0ddc2c95c728b829e8971248a0f7
Mario Michel <1108mario@gmail.com>
2019-08-24 02:00:21 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 date=$(date +%d%m%y)
 
+# Stats
 currentsizearchive=$(du -sh ./Archive --exclude=./Archive/resources | awk '{print $1}')B
 totalconfigsarchive=$(tree | grep .conf | wc -l)
 totallines=$(find ./Archive -name "*.conf" -mmin -120 -exec cat {} + | wc -l)
 configsinarchive=$(find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -120 | wc -l)
-avglinesinconfig=$(expr $totallines / $configsinarchive)
 fortinethostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Fortinet/Fortinet-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 hphostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/HP/HP-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/Cisco-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
@@ -14,9 +14,11 @@ dirsinarchive=$(find ./Archive -maxdepth 1 -type d | wc -l)
 minus2=2 # Caused by also counting the ./Archive and ./Archive/resources as a folder
 minus1=1 # Caused by also counting the./Archive/resources/config.php as a .conf file
 
-total=$(expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount)
-dirsinarchive=$(expr $dirsinarchive - $minus2)
-totalconfigsarchive=$(expr $totalconfigsarchive - $minus1)
+# Quick Math
+avglinesinconfig=$((expr $totallines / $configsinarchive))
+total=$((expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount))
+dirsinarchive=$((expr $dirsinarchive - $minus2))
+totalconfigsarchive=$((expr $totalconfigsarchive - $minus1))
 
 echo "------------------------------------------------"
 echo "Fortinet hosts in host file .......... $fortinethostfilecount"
