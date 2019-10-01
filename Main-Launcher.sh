286865b6a1122cb9815a0043a5a4166d3645711f
Mario Michel <1108mario@gmail.com>
2019-10-01 17:00:37 +0200

    Update Main-Launcher.sh

--- Main-Launcher.sh
+++ Main-Launcher.sh
@@ -9,8 +9,8 @@ echo "[i]: Start of backup @ $(date +%H:%m)" >> ./Log/Backup/log$date.txt
 ### Backup Modules ###
 # ./Modules/Backup/Fortinet/Fortinet.sh &>> ./Log/Fortinet/log$date.txt ### FUNCTIONAL ###
 # ./Modules/Backup/Fortinet/Fortinet-Special.sh &>> ./Log/Fortinet/log$date.txt ### Disabled by default FUNCTIONAL ###
-# ./Modules/Backup/Cisco/Cisco.sh &>> ./Log/Cisco/log$date.txt ### Only for sgx00 series working ###
-# ./Modules/Backup/DELL/DELL.sh &>> ./Log/DELL/log$date.txt ### NOT YET FUNCTIONAL! ###
+# ./Modules/Backup/Cisco/Cisco.sh &>> ./Log/Cisco/log$date.txt ### Only for sgx00 & n3XXX series working ###
+# ./Modules/Backup/DELL/DELL.sh &>> ./Log/DELL/log$date.txt ### Only for nXXXX series working ###
 # ./Modules/Backup/HP/HP.sh &>> ./Log/HP/log$date.txt ### NOT YET FUNCTIONAL! ###
 
 ### Archive Compression/Debugs & Stats ###
