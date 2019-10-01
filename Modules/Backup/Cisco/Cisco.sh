52737897b1a9c52ae9b5f2ef00b42bbb6c3ea6b1
Mario Michel <1108mario@gmail.com>
2019-10-01 16:59:56 +0200

    Update Cisco.sh

--- Modules/Backup/Cisco/Cisco.sh
+++ Modules/Backup/Cisco/Cisco.sh
@@ -2,32 +2,5 @@
 
 echo "[i]: Started Backup of Configs : CISCO"
 
-for device in $(egrep -v "^\s*(#|$)" ./Devices/Cisco/sgX00.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
- do
-  echo -e "[i]: Host --> $device"
-  if ping -c 3 $device &> /dev/null
-   then
-    echo "[i]: $device reachable"
-    ./Modules/Backup/Cisco/sgX00.sh $device
-    name=$(grep hostname ./BackupConfigCisco | sed 's|["?]||g' | sed 's/hostname //')
-    if [ -z "$name" ]
-     then
-      echo "[i]: $device Name not found"
-     else
-      echo "[i]: $device Name found $name"
-      mkdir -v ./Archive/$name
-      date=$(date +"%H-%M_%d-%m-%Y")
-      mv -v ./BackupConfigCisco ./Archive/$name/$name-$date.conf
-      if [ -f ./Archive/$name/$name-$date.conf ]
-       then
-        echo "[i]: File $name-$date.conf found"
-        echo "[i]: $device backup succeeded"
-       else
-        echo "[i]: File $name-$date.conf not found"
-        echo "[i]: $device backup failed"
-      fi
-    fi
-    else
-    echo "[i]: $device not reachable"
- fi
-done
+# ./Modules/Backup/Cisco/sgX00.sh
+# ./Modules/Backup/Cisco/n3XXX.sh
