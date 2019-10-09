4c2029b5b81301df9af0637eeec4f73ab9bf8522
Mario Michel <1108mario@gmail.com>
2019-10-09 14:31:45 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Stats
+# Get Stats
 currentsizearchive=$(du -sh ./Archive --exclude=./Archive/resources | awk '{print $1}')B
 totalconfigsarchive=$(tree | grep .conf | wc -l)
 totallines=$(find ./Archive -name "*.conf" -mmin -120 -exec cat {} + | wc -l)
 configsinarchive=$(find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -120 | wc -l)
 fortinethostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Fortinet/Fortinet-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 hphostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/HP/HP-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
-ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/Cisco-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
-dellhostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/DELL/DELL-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/n3XXX-txt ./Devices/Cisco/sgX00.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+dellhostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/DELL/nXXXX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 dirsinarchive=$(find ./Archive -maxdepth 1 -type d | wc -l)
 minus2=2 # Caused by also counting the ./Archive and ./Archive/resources as a folder
 minus1=1 # Caused by also counting the./Archive/resources/config.php as a .conf file
 
-# Quick Math
+# Do some Quick Math 2 + 2 = 4 - 1 = 3 
 avglinesinconfig=$((expr $totallines / $configsinarchive))
 total=$((expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount))
 dirsinarchive=$((expr $dirsinarchive - $minus2))
 totalconfigsarchive=$((expr $totalconfigsarchive - $minus1))
 
+# Ouput Stats 
 echo "------------------------------------------------"
 echo "Fortinet hosts in host file .......... $fortinethostfilecount"
 echo "HP hosts in host file ................ $hphostfilecount"
