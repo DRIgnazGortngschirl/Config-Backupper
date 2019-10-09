2240fa58bcf0fb1e56a6a6c41bf5e8d48235474a
Mario Michel <1108mario@gmail.com>
2019-10-09 14:09:40 +0200

    Update Archiver.sh

--- Modules/Archive/OldConfigs/Archiver.sh
+++ Modules/Archive/OldConfigs/Archiver.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
 # Use gunzip -r ./Archive/ to unzip all ziped .gz files
+
+echo "[i]: Compressing configs Started"
+find ./Archive -name "*.conf" -mtime +PLACEHOLDERFORARCHIVETIMEARCHIVECOMMPRESS -exec gzip -v {} +
+echo "[i]: Compressing configs End"
+echo "[i]: Deleting configs Started"
+find ./Archive -name "*.conf.gz" -mtime +PLACEHOLDERFORARCHIVETIMEARCHIVEDELETE -exec rm -v {} +
+echo "[i]: Deleting configs End"
