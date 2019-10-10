cd119acd2bd409852753aef904b0847aad157682
Mario Michel <1108mario@gmail.com>
2019-10-10 10:13:18 +0200

    Update CleanUp.sh

--- Modules/Clean/Configs/CleanUp.sh
+++ Modules/Clean/Configs/CleanUp.sh
@@ -11,6 +11,6 @@ if [[ $REPLY =~ ^[Y]$ ]]
             then
                 echo "I will DELETE now ALL Backuped Configs."
                 sleep 3
-                find ./Archive ! -name '.src' ! -name '*.js' ! -name 'kunden.json' ! -name 'main.css'  ! -name 'css'  ! -name 'js'  ! -name 'index.php'  ! -name '*json'  ! -name 'Archive' -exec rm -v  -Rf {} +
+                find ./Archive ! -name 'index.php' ! -name 'DirectoryLister.php' ! -name '*js' ! -name '*css' ! -name '*.php' ! -name '*png' ! -name '*img'  ! -name 'bootstrap' ! -name 'themes'  ! -name 'resources' ! -name 'Archive' -exec rm -v  -Rf {} +
         fi
 fi
