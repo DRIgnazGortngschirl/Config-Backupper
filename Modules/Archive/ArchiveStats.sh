fbe63e4290971f7400de6a2ebe3c0e97e243e841
Mario Michel <1108mario@gmail.com>
2019-10-10 13:42:16 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -5,9 +5,10 @@ currentsizearchive=$(du -sh ./Archive --exclude=./Archive/resources | awk '{prin
 totalconfigsarchive=$(tree | grep .conf | wc -l)
 totallines=$(find ./Archive -name "*.conf" -mmin -120 -exec cat {} + | wc -l)
 configsinarchive=$(find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -120 | wc -l)
-fortinethostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Fortinet/Fortinet-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+fortinethostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Fortinet/fortigateX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+fortinetspecialhostfilecount=$(egrep -v "^\s*(#|$)" ./Modules/Backup/fortigateX-special.sh | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort | uniq | wc -l)
 hphostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/HP/HP-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
-ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/n3XXX-txt ./Devices/Cisco/sgX00.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/n3XXX-txt ./Devices/Cisco/sgX00.txt ./Devices/Cisco/wsc3650-txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 dellhostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/DELL/nXXXX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 dirsinarchive=$(find ./Archive -maxdepth 1 -type d | wc -l)
 minus2=2 # Caused by also counting the ./Archive and ./Archive/resources as a folder
@@ -15,7 +16,8 @@ minus1=1 # Caused by also counting the./Archive/resources/config.php as a .conf 
 
 # Do some Quick Math 2 + 2 = 4 - 1 = 3 
 avglinesinconfig=$((expr $totallines / $configsinarchive))
-total=$((expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount))
+fortinethostfilecounttotal$((expr $fortinethostfilecount + fortinetspecialhostfilecount))
+total=$((expr $fortinethostfilecounttotal + $ + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount))
 dirsinarchive=$((expr $dirsinarchive - $minus2))
 totalconfigsarchive=$((expr $totalconfigsarchive - $minus1))
 
