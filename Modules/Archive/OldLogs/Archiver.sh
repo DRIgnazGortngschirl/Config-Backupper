03bf0155668eceebae44bd511d8e77828dd9b4d1
Mario Michel <1108mario@gmail.com>
2019-10-10 09:16:52 +0200

    Update Archiver.sh

--- Modules/Archive/OldLogs/Archiver.sh
+++ Modules/Archive/OldLogs/Archiver.sh
@@ -3,8 +3,8 @@
 # Use gunzip -r ./Log/ to unzip all ziped .gz files
 
 echo "[i]: Compressing logs Started"
-find ./Log -mtime +PLACEHOLDERFORARCHIVETIMELOGSCOMMPRESS -exec gzip -v {} +
+find ./Log -name "log*.txt" -mtime +PLACEHOLDERFORARCHIVETIMELOGSCOMMPRESS -exec gzip -v {} +
 echo "[i]: Compressing logs End"
 echo "[i]: Deleting logs Started"
-find ./Log -mtime +PLACEHOLDERFORARCHIVETIMELOGSDELETE -exec gzip -v {} +
+find ./Log -name "log*.txt.gz" -mtime +PLACEHOLDERFORARCHIVETIMELOGSDELETE -exec rm -v {} +
 echo "[i]: Deleting logs End"
