22d8011f92d96c11a3cc529379915f7ca5dea5ca
Mario Michel <1108mario@gmail.com>
2019-10-14 09:29:36 +0200

    Update nXXXX.sh

--- Modules/Backup/DELL/nXXXX.sh
+++ Modules/Backup/DELL/nXXXX.sh
@@ -4,22 +4,24 @@ user="dell"
 passwd="XXXXXXPASSWORDXXXXXX"
 
 echo "[i]: Started Backup of Configs : DELL (nXXXX.sh)"
-for device in $(egrep -v "^\s*(#|$)" ./Devices/DELL/NXXXX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+for device in $(egrep -v "^\s*(#|$)" ./Devices/DELL/nXXXX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
  do
   echo -e "[i]: Host --> $device"
-  sshpass -p "$passwd" \ssh -v $user@$device <<EOF > BackupConfigDELLTEMP1 
-   enable
-   show running-config 
-   exit
-   exit
-   exit
+  if ping -c 3 $device &> /dev/null
+   then
+     echo "[i]: $device reachable"
+     sshpass -p "$passwd" \ssh -v $user@$device <<EOF > BackupConfigDELLTEMPnXXXX
+     enable
+     show running-config
+     exit
+     exit
+     exit
 EOF
-   name=$(grep "hostname" BackupConfigDELLTEMP1 | sed 's|["?]||g' | sed 's/hostname //' |  tr -dc '[:print:]')
-   grep -v "$name>" BackupConfigDELLTEMP1 | grep -v "$name#"> BackupConfigDELL
+   name=$(grep "hostname" BackupConfigDELLTEMPnXXXX | sed 's|["?]||g' | sed 's/hostname //' |  tr -dc '[:print:]')
+   grep -v "$name>" BackupConfigDELLTEMPnXXXX | grep -v "$name#"> BackupConfigDELLnXXXX
    mkdir -v Archive/$name
    date=$(date +"%H-%M_%d-%m-%Y")
-   mv -v BackupConfigDELL ./Archive/$name/$name-$date.conf
-   rm BackupConfigDELLTEMP1
+   mv -v BackupConfigDELLnXXXX ./Archive/$name/$name-$date.conf
    if [ -f ./Archive/$name/$name-$date.conf ]
     then
      echo "[i]: File $name-$date.conf found!"
@@ -28,4 +30,8 @@ EOF
      echo "[i]: File $name-$date.conf not found!"
      echo "[i]: $device backup failed"
    fi
+ fi
+   rm -v BackupConfigDELLTEMPnXXXX
+     else
+    echo "[i]: $device not reachable"
 done
