9a7a42833a531df439050f17fd82c6129f50b0a6
Mario Michel <1108mario@gmail.com>
2019-10-16 08:29:09 +0200

    Update Fastdebug.sh

--- Modules/Archive/Fastdebug.sh
+++ Modules/Archive/Fastdebug.sh
@@ -6,17 +6,17 @@ echo "Backup Errors"
 echo "Fortinet:"
 echo "--------------------------------------------------------------------------------------------------"
 join <(sort ./Devices/Fortinet/fortigateX.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
-join <(sort ./Modules/Backup/fortigateX-special.sh | grep '#' | grep -v '#!/bin/bash' | sed 's/# //') <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
+join <(sort ./Modules/Backup/Fortinet/fortigateX-special.sh | grep '#' | grep -v '#!/bin/bash' | sed 's/# //') <(sort ./Log/Fortinet/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
 echo "--------------------------------------------------------------------------------------------------"
 echo "Cisco:"
 echo "--------------------------------------------------------------------------------------------------"
 join <(sort ./Devices/Cisco/sgX00.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
-join <(sort ./Devices/Cisco/n3XXX-txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
-join <(sort ./Devices/Cisco/wsc3650-txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
+join <(sort ./Devices/Cisco/n3XXX.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
+join <(sort ./Devices/Cisco/wsc3650.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
 echo "--------------------------------------------------------------------------------------------------"
 echo "DELL:"
 echo "----------------------------------------------------------------------------"
-join <(sort ./Devices/DELL/nXXXX-txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
+join <(sort ./Devices/DELL/nXXXX.txt | egrep -v "^\s*(#|$)") <(sort ./Log/Cisco/log$date.txt | grep '\[i]:' | grep -e "Name not found" -e "not reachable" -e "backup failed" | sed -e 's/\[i]: //')
 echo "----------------------------------------------------------------------------"
 #echo "HP :"
 #echo "----------------------------------------------------------------------------"
