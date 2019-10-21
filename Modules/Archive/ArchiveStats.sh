290c446bb5379da0c59ded514771adc9be4221df
Mario Michel <1108mario@gmail.com>
2019-10-21 09:21:27 +0200

    Update ArchiveStats.sh

--- Modules/Archive/ArchiveStats.sh
+++ Modules/Archive/ArchiveStats.sh
@@ -8,18 +8,18 @@ configsinarchive=$(find ./Archive -maxdepth 2 -type f -name "*.conf" -mmin -120 
 fortinethostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Fortinet/fortigateX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 fortinetspecialhostfilecount=$(egrep -v "^\s*(#|$)" ./Modules/Backup/fortigateX-special.sh | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort | uniq | wc -l)
 hphostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/HP/HP-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
-ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/n3XXX-txt ./Devices/Cisco/sgX00.txt ./Devices/Cisco/wsc3650-txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
+ciscohostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/Cisco/n3XXX.txt ./Devices/Cisco/sgX00.txt ./Devices/Cisco/wsc3650.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 dellhostfilecount=$(egrep -v "^\s*(#|$)" ./Devices/DELL/nXXXX.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 dirsinarchive=$(find ./Archive -maxdepth 1 -type d | wc -l)
 minus2=2 # Caused by also counting the ./Archive and ./Archive/resources as a folder
 minus1=1 # Caused by also counting the./Archive/resources/config.php as a .conf file
 
 # Do some Quick Math 2 + 2 = 4 - 1 = 3 
-avglinesinconfig=$((expr $totallines / $configsinarchive))
-fortinethostfilecounttotal=$((expr $fortinethostfilecount + $fortinetspecialhostfilecount))
-total=$((expr $fortinethostfilecounttotal + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount))
-dirsinarchive=$((expr $dirsinarchive - $minus2))
-totalconfigsarchive=$((expr $totalconfigsarchive - $minus1))
+avglinesinconfig=$(expr $totallines / $configsinarchive)
+fortinethostfilecounttotal=$(expr $fortinethostfilecount + $fortinetspecialhostfilecount)
+total=$(expr $fortinethostfilecounttotal + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount)
+dirsinarchive=$(expr $dirsinarchive - $minus2)
+totalconfigsarchive=$(expr $totalconfigsarchive - $minus1)
 
 # Ouput Stats 
 echo "------------------------------------------------"
