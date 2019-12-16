c747825c1cde9c9b54bfc7c89775fb5f06ef31b7
Mario Michel <1108mario@gmail.com>
2019-12-16 16:37:31 +0100

    Create switch-vlan-checker.sh

---
+++ Modules/Other/Fortinet/switch-vlan-checker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE=$1
+if [ -z $FILE ]; then
+    echo "[error]: input file was not declared"
+    echo "[i]: Use ./$(basename $0) ./Archive/<PATH TO CONFIG FILE>"
+    exit
+fi
+
+./backend.sh $FILE > switch-vlan-checker-temp
+cat switch-vlan-checker-temp | sed '/^$/d'
+rm switch-vlan-checker-temp
